4a2674810caf627393d574485bc10ac79bf6073a
Jesse Futerman <jessefuterman@gmail.com>
2019-04-09 22:08:38 -0400

    first commit

---
+++ src/component/drumpatterncontainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import SequenceStep from "./sequencestep.js"
+import '../App.css';
+class DrumPatternContainer  extends Component {
+    render() {
+      return (
+        <div className="DrumPatternContainer">
+         
+        <SequenceStep className = "SequenceStepLook"> daddi like</SequenceStep>
+        
+
+
+        </div>
+        
+      );
+    }
+  }
+
+export default DrumPatternContainer; 
---
+++ src/component/sequencestep.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class SequenceStep  extends Component {
+    render() {
+      return (
+        <div className="SequenceStep">
+          <h1> hello </h1>
+         
+
+
+        </div>
+        
+      );
+    }
+  }
+
+export default SequenceStep; 
